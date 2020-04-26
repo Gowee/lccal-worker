093e485becaa89e6eee66d7c17ae281bd57d2b42
Hung-I Wang <whygowe@gmail.com>
2020-04-26 22:31:56 +0800

    Just works

---
+++ src/calgen.ts
@@ -0,0 +1,23 @@
+import { Contest, fetchContests } from './lcapi';
+import ICalGenerator from 'ical-generator';
+
+const LCURL = "https://leetcode.com/"
+
+function urlJoin(base: string, url: string): string {
+    const absUrl = new URL(url, base);
+    return absUrl.toString();
+}
+
+export function generateCalendar(contests: Array<Contest>, limit=10): string {
+    const cal = ICalGenerator({ domain: "lccal-worker", name: "LeetCode Contests", url: "https://leetcode.com/contest" });
+    for (const contest of contests.slice(0, limit)) {
+        cal.createEvent({
+            id: contest.titleSlug,
+            start: new Date(contest.startTime * 1000),
+            end: new Date((contest.startTime + contest.duration) * 1000),
+            summary: `[Leetcode] ${contest.title}`,
+            url: urlJoin(LCURL, contest.titleSlug)
+        });
+    }
+    return cal.toString();
+}
---
+++ src/handler.ts
@@ -0,0 +1,13 @@
+import { fetchContests } from './lcapi';
+import { generateCalendar } from './calgen';
+
+const ICALHEADERS = {
+  'Contest-Type': "text/calendar; charset=utf-8",
+  'Content-Disposition': `attachment; filename="leetcode-contests.ical"`
+};
+
+export async function handleRequest(request: Request): Promise<Response> {
+  const contests = await fetchContests();
+  const ical = generateCalendar(contests);
+  return new Response(ical, { headers: ICALHEADERS })
+}
